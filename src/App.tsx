263b5c66f53b0014f33061885ac6769a6594f8f9
shlok1201 <135605946+shlok1201@users.noreply.github.com>
2023-12-16 10:13:47 +0000

    ability to add new todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+interface Todo {
+  id: number;
+  value: string;
+}
 
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [value, setValue] = useState("")
+
+  const addTodo = () => {
+    const newTodo = {
+      id: todos.length,
+      value, 
+    }
+
+    setTodos((prev) => [...prev, newTodo])
+    setValue("")
+  }
+  
   return (
     <div>
-Hello
-    <div/>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        addTodo()
+      }}>
+        <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+        <button type='submit'>Add</button>
+      </form>
+      <div>
+        {todos.map(t => (
+          <div key={t.id}>
+            <p>{t.value}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
